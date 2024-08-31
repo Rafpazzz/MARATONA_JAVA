478defc7b8b997a9eb65e5d266de5db2d622b4df
Rafpazzz <rafael_mourapaz@hotmail.com>
2024-08-30 22:31:17 -0300

    Update Aula05EstruturasCondicionas.java

--- src/academy/devdojo/maratona_java/introducao/Aula05EstruturasCondicionas.java
+++ src/academy/devdojo/maratona_java/introducao/Aula05EstruturasCondicionas.java
@@ -2,8 +2,14 @@ package academy.devdojo.maratona_java.introducao;
 
 public class Aula05EstruturasCondicionas {
     public static void main(String[] args) {
-        if(true){
-            System.out.println("Dentro do if");
+        int idade = 18;
+
+        if(idade < 15){
+            System.out.println("Categoria infantil");
+        }if(idade <18 && idade >= 15){
+            System.out.println("Categoria juvenil");
+        }else{
+            System.out.println("Categoria adulto");
         }
     }
 }
