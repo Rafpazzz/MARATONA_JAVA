ca822eb87c3291ea134c7db44a90b4d8d5255233
Rafpazzz <rafael_mourapaz@hotmail.com>
2024-09-03 11:06:36 -0300

    arrays_multidimencionais

---
+++ src/academy/devdojo/maratona_java/introducao/Aula08ArraysMultidimencionas01.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratona_java.introducao;
+
+public class Aula08ArraysMultidimencionas01 {
+    public static void main(String[] args) {
+        int[][] dias = new int[3][3];
+
+    }
+}
