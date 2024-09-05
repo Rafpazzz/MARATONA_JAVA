4188edac9efbe449fc51755f2c32d8ba77f7e860
Rafpazzz <rafael_mourapaz@hotmail.com>
2024-09-04 21:57:11 -0300

    cvbhnj

---
+++ src/academy/devdojo/maratona_java/javacore/Bintroducaometodos/teste/CalculadoraTeste02.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratona_java.javacore.Bintroducaometodos.teste;
+
+import academy.devdojo.maratona_java.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTeste02 {
+    public static void main(String[] args) {
+        Calculadora calcular = new Calculadora();
+        int a = 1;
+        int b = 2;
+
+        calcular.alteraDoisNumeros(a, b);
+        System.out.println("Dntro do CalculadoraTeste02");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
