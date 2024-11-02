c7c30e59b0451be0066041641785b14b85f35831
Rafael Paz <rafael_mourapaz@hotmail.com>
2024-11-02 15:14:10 -0300

    Modificador Static

---
+++ src/academy/devdojo/maratona_java/javacore/Fmodificadoresestaticos/test/Testcarro.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratona_java.javacore.Fmodificadoresestaticos.test;
+
+import academy.devdojo.maratona_java.javacore.Fmodificadoresestaticos.domain.Carro;
+
+public class Testcarro {
+    public static void main(String[] args) {
+        Carro c1 = new Carro("BMW", 280);
+        Carro c2 = new Carro("Mercedez", 275);
+        Carro c3 = new Carro("Audi", 285);
+
+        Carro.velocidadeLimite = 180;
+
+        c1.imprime();
+        c2.imprime();
+        c3.imprime();
+    }
+}
