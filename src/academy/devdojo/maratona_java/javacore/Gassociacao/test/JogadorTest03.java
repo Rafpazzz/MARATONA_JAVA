872fe7736a592eeebba1adc5959cbbe56d758a8d
Rafael Paz <rafael_mourapaz@hotmail.com>
2025-03-05 14:23:54 -0300

    revisao

--- src/academy/devdojo/maratona_java/javacore/Gassociacao/test/JogadorTest03.java
+++ src/academy/devdojo/maratona_java/javacore/Gassociacao/test/JogadorTest03.java
@@ -6,8 +6,9 @@ import academy.devdojo.maratona_java.javacore.Gassociacao.domain.Time;
 public class JogadorTest03 {
     public static void main(String[] args) {
         Jogador j1 = new Jogador("Cafú");
+        Jogador j2 = new Jogador("Pele");
         Time t1 = new Time("Brasil");
-        Jogador[] jogadores = {j1};
+        Jogador[] jogadores = {j1, j2};
 
         j1.setTime(t1);
 
@@ -18,5 +19,6 @@ public class JogadorTest03 {
 
         System.out.println("---time---");
         t1.imprime();
+        t1.getJogadores();
     }
 }
